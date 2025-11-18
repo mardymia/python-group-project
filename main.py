324c6aa10765e622f57a4db22db916156eaf7a8e
mardymia <mardysoto05@gmail.com>
2025-11-18 14:08:58 -0500

    Quick error fix (18-Nov-2025-14:08:58)

--- main.py
+++ main.py
@@ -219,9 +219,6 @@ def __MAIN():
                         print("\nInvalid input!\n")
                 else:
                     print("\nInvalid input!\n")
-
-
-
         elif userChoice == 4:
             writeResult = writeConfigToFile()
             if writeResult < -1:
